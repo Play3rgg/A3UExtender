6e84f0c7fc4f10386df02327ea7bcabbfdde3fc4
Play3rgg <39414270+Play3rgg@users.noreply.github.com>
2025-05-26 13:50:58 +0300

    delete excess civ/reb/riv

--- a3au_low_air/mod.cpp
+++ a3au_low_air/mod.cpp
@@ -1,5 +1,5 @@
-name = "A3 Antistasi Ultimate Extender example";
-author = "A3-Antistasi dev team, A3-Antistasi Ultimate dev team";
-tooltip = "Antistasi Ultimate Extender example";
+name = "A3 Antistasi Ultimate Extender Low Air Templates";
+author = "A3-Antistasi dev team, A3-Antistasi Ultimate dev team, Play3rgg";
+tooltip = "Antistasi Ultimate Extender Low Air Templates";
 actionName = "GitHub";
-action = "https://github.com/Westalgie/A3UExtender"
+action = "https://github.com/Play3rgg/A3UExtender/tree/master"
